a958488b54e2cf5e73e19449efd01e50ef1570fb
HuaZhenQiang <irajude.tajanlangit@wvsu.edu.ph>
2023-12-13 00:58:03 +0800

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -81,11 +81,11 @@
     /* Links on mouse-over */
     .sidebar a:hover:not(.active) {
       background-color: #002a5b;
-    /* background-color: #555;*/
-    color: white;
-  }
+      /* background-color: #555;*/
+      color: white;
+    }
 
-div.content {
+    div.content {
       margin-left: 250px;
       padding: 1px 16px;
       min-height: 100vh;
@@ -127,10 +127,10 @@ div.content {
 
       div.content {
         margin-left: 0;
-  }
-}
+      }
+    }
 
-</style>
+  </style>
 
 </head>
 
@@ -152,83 +152,124 @@ div.content {
   <div class="sidebar">
 
     <a href="#dashboard.php">
-           <i class="fas fa-home"></i>
-            <span class="nav-item">Dashboard</span>
-          </a>
-    <a href="#announcement.php">
-            <i class="fas fa-bullhorn"></i>
-            <span class="nav-item">Announcement</span>
-          </a>
-          <a href="#officials.php">
-            <i class="fas fa-user"></i>
-            <span class="nav-item">Officials</span>
-          </a>
-    <a href="#Issuance.php">
-            <i class="fas fa-id-card"></i>
-            <span class="nav-item">Issuance Details</span>
-          </a>
-          <a href="#contentM.php">
-            <i class="fas fa-database"></i>
-            <span class="nav-item">Content Management</span>
-          </a>
-    <a href="#records.php">
-            <i class="fab fa-dochub"></i>
-            <span class="nav-item">Records</span>
-          </a>
-          <a href="#request.php">
-            <i class="fas fa-folder-open"></i>
-            <span class="nav-item">Monitoring of Request</span>
-          </a>
-    <a href="#setting.php">
-            <i class="fas fa-cog"></i>
-            <span class="nav-item">Setting</span>
-          </a>
-          <a href="#help.php">
-            <i class="fas fa-question-circle"></i>
-            <span class="nav-item">Help</span>
-          </a>
-    <a href="#logout.php">
-            <i class="fas fa-sign-out-alt"></i>
-            <span class="nav-item">Logout</span>
-          </a>
+     <i class="fas fa-home"></i>
+     <span class="nav-item">Dashboard</span>
+   </a>
+   <a href="#announcement.php">
+    <i class="fas fa-bullhorn"></i>
+    <span class="nav-item">Announcement</span>
+  </a>
+  <a href="#officials.php">
+    <i class="fas fa-user"></i>
+    <span class="nav-item">Officials</span>
+  </a>
+  <a href="#issuanceDet.php">
+    <i class="fas fa-id-card"></i>
+    <span class="nav-item">Issuance Details</span>
+  </a>
+  <a href="#contentM.php">
+    <i class="fas fa-database"></i>
+    <span class="nav-item">Content Management</span>
+  </a>
+
+  
+
+
+  <!-- Records Dropdown -->
+<div class="dropdown">
+  <a class="dropdown-toggle" href="#" role="button" id="recordsDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <i class="fab fa-dochub"></i>
+    <span class="nav-item">Records</span>
+  </a>
+  <div class="dropdown-menu dropdown-menu-right" aria-labelledby="recordsDropdown">
+    <a class="dropdown-item record-link" href="#record1.php">Record 1</a>
+    <a class="dropdown-item record-link" href="#record2.php">Record 2</a>
+    <!-- Add more dropdown items as needed -->
   </div>
+</div>
+<!-- End Records Dropdown -->
 
-  <!-- Page content -->
-  <div class="content">
-    Dashboard |  
-  </div>
+<script>
+  // JavaScript to add 'active' class to the corresponding dropdown item
+  document.addEventListener("DOMContentLoaded", function () {
+    var currentLocation = window.location.href;
 
-  <!-- Script for time -->
-  <script>
-    function startTime() {
-      var today = new Date();
-      var day = today.getDate();
-      var monthNames = ["January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"];
-      var month = monthNames[today.getMonth()];
-      var year = today.getFullYear();
-      var h = today.getHours();
-      var m = today.getMinutes();
-      var s = today.getSeconds();
-      var ampm = h >= 12 ? 'pm' : 'am';
-
-      if (h > 12) {
-        h = h - 12;
-      } else if (h == 0) {
-        h = 12;
+    // Check each dropdown item and add 'active' class if its href matches the current location
+    document.querySelectorAll('.record-link').forEach(function (item) {
+      if (item.getAttribute('href') === currentLocation) {
+        item.classList.add('active');
       }
+    });
+  });
+</script>
 
-      m = checkTime(m);
-      s = checkTime(s);
-      document.getElementById('txt').innerHTML = month + " " + day + ", " + year + " <br> " + h + ":" + m + ":" + s + " " + ampm;
-      var t = setTimeout(startTime, 500);
+<style>
+  /* Custom CSS for dropdown highlight effect */
+  .dropdown-item:hover,
+  .dropdown-item:focus,
+  .record-link.active {
+    background-color: #002a5b;
+    color: white;
+  }
+</style>
+
+
+
+
+  <a href="#request.php">
+    <i class="fas fa-folder-open"></i>
+    <span class="nav-item">Monitoring of Request</span>
+  </a>
+  <a href="#setting.php">
+    <i class="fas fa-cog"></i>
+    <span class="nav-item">Setting</span>
+  </a>
+  <a href="#help.php">
+    <i class="fas fa-question-circle"></i>
+    <span class="nav-item">Help</span>
+  </a>
+  <a href="#logout.php">
+    <i class="fas fa-sign-out-alt"></i>
+    <span class="nav-item">Logout</span>
+  </a>
+</div>
+
+<!-- Page content -->
+<div class="content">
+  Dashboard |  
+</div>
+
+<!-- Script for time -->
+<script>
+  function startTime() {
+    var today = new Date();
+    var day = today.getDate();
+    var monthNames = ["January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"];
+    var month = monthNames[today.getMonth()];
+    var year = today.getFullYear();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    var ampm = h >= 12 ? 'pm' : 'am';
+
+    if (h > 12) {
+      h = h - 12;
+    } else if (h == 0) {
+      h = 12;
     }
 
-    function checkTime(i) {
-      if (i < 10) { i = "0" + i };
-      return i;
-    }
-  </script>
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML = month + " " + day + ", " + year + " <br> " + h + ":" + m + ":" + s + " " + ampm;
+    var t = setTimeout(startTime, 500);
+  }
+
+  function checkTime(i) {
+    if (i < 10) { i = "0" + i };
+    return i;
+  }
+</script>
 
 </body>
 </html>
