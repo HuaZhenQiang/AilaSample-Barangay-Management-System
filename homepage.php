da7dcc4def4b3014f31d823dcd7a5a997c356056
HuaZhenQiang <irajude.tajanlangit@wvsu.edu.ph>
2023-12-12 22:01:58 +0800

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  
+
   <title>Barangay Management System</title>
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" />
@@ -53,8 +53,7 @@
     /* The side navigation menu */
     .sidebar {
       margin: 60px 0 0 0;
-      /* Adjusted to accommodate the top bar */
-      padding: 0;
+      padding: 0; /* Adjusted to accommodate the top bar */
       width: 200px;
       background-color: #ffffff;
       position: fixed;
@@ -74,7 +73,7 @@
     .sidebar a.active {
       background-color: #002a5b;
 /*      background-color: #04AA6D;*/
-color: white;
+      color: white;
 }
 
 /* Links on mouse-over */
@@ -84,9 +83,8 @@ color: white;
 color: white;
 }
 
-/* Page content. The value of the margin-left property should match the value of the sidebar's width property */
 div.content {
-  margin-left: 220px;
+  margin-left: 200px;
   /* Adjusted to accommodate the sidebar width */
   padding: 1px 16px;
   height: 1000px;
@@ -94,7 +92,7 @@ div.content {
 }
 
 /* On screens that are less than 700px wide, make the sidebar into a topbar */
-@media screen and (max-width: 700px) {
+@media screen and (max-width: 1000px) {
   .sidebar {
     width: 100%;
     height: auto;
@@ -106,7 +104,7 @@ div.content {
 }
 
 /* On screens that are less than 400px, display the bar vertically, instead of horizontally */
-@media screen and (max-width: 400px) {
+@media screen and (max-width: 500px) {
   .sidebar a {
     text-align: center;
     float: none;
@@ -117,7 +115,7 @@ div.content {
 </head>
 
 <body style="background: url('img/') no-repeat center center fixed; background-size: 100%;" onload="startTime()">
-  
+
   <!-- Top Navigation Bar -->
   <div class="header">
     <img src="img/logo.png" alt="Iloilo City Seal">
@@ -126,63 +124,56 @@ div.content {
       <span class="d-none d-md-block">Jaro, Iloilo City</span>
       <span class="d-none d-sm-none">Jaro, Iloilo City</span>
     </div>
+    <div id="txt" style="color: white; margin-left: auto; text-align: center;"></div>
+  </div>
 
-    <div id="txt" style="color: white; margin-left: auto; text-align: center;"></div> <!-- Add this line for displaying date and time -->
+  <!-- Sidebar -->
+  <div class="sidebar">
+    <a href="#Dashboard">Dashboard</a>
+    <a href="#Announcement">Announcement</a>
+    <a href="#Officials">Officials</a>
+    <a href="#Issuance">Issuance Details</a>
+    <a href="#ContentM">Content Management</a>
+    <a href="#Records">Records</a>
+    <a href="#Request">Monitoring of Request</a>
+  </div>
 
-   <!--  <a href="#home">Home</a>
-    <a href="#news">News</a>
-    <a href="#contact">Contact</a>
-    <a href="#about">About</a> -->
-</div>
+  <!-- Page content -->
+  <div class="content">
+    Dashboard |  
+  </div>
 
-<!-- Sidebar -->
-<div class="sidebar">
-  <a href="#Dashboard">Dashboard</a>
-  <a href="#Announcement">Announcement</a>
-  <a href="#Officials">Officials</a>
-  <a href="#Issuance">Issuance Details</a>
-  <a href="#ContentM">Content Management</a>
-  <a href="#Records">Records</a>
-  <a href="#Request">Monitoring of Request</a>
-</div>
+  <!-- Script for time -->
+  <script>
+    function startTime() {
+      var today = new Date();
+      var day = today.getDate();
+      var monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
+      var month = monthNames[today.getMonth()];
+      var year = today.getFullYear();
+      var h = today.getHours();
+      var m = today.getMinutes();
+      var s = today.getSeconds();
+      var ampm = h >= 12 ? 'pm' : 'am';
 
-<!-- Page content -->
-<div class="content">
-  Dashboard |  
-</div>
+      if (h > 12) {
+        h = h - 12;
+      } else if (h == 0) {
+        h = 12;
+      }
 
-
-<!-- Script for time -->
-<script>
-  function startTime() {
-    var today = new Date();
-    var day = today.getDate();
-    var monthNames = ["January", "February", "March", "April", "May", "June",
-      "July", "August", "September", "October", "November", "December"];
-    var month = monthNames[today.getMonth()];
-    var year = today.getFullYear();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    var ampm = h >= 12 ? 'pm' : 'am';
-
-    if (h > 12) {
-      h = h - 12;
-    } else if (h == 0) {
-      h = 12;
+      m = checkTime(m);
+      s = checkTime(s);
+      document.getElementById('txt').innerHTML = month + " " + day + ", " + year + " <br> " + h + ":" + m + ":" + s + " " + ampm;
+      var t = setTimeout(startTime, 500);
     }
 
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('txt').innerHTML = month + " " + day + ", " + year + " <br> " + h + ":" + m + ":" + s + " " + ampm;
-    var t = setTimeout(startTime, 500);
-  }
-
-  function checkTime(i) {
-    if (i < 10) { i = "0" + i };
-    return i;
-  }
-</script>
+    function checkTime(i) {
+      if (i < 10) { i = "0" + i };
+      return i;
+    }
+  </script>
 
 </body>
 </html>
