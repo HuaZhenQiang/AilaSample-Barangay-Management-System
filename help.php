cbe04d1312bc5a084db50cb434698518645fb1b7
HuaZhenQiang <irajude.tajanlangit@wvsu.edu.ph>
2024-01-10 07:59:21 +0800

    Update help.php

--- help.php
+++ help.php
@@ -1,234 +1,299 @@
+<!-- welcome with emoji -->
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
+  <title>Animated Login Form</title>
+  
+  <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/a81368914c.js"></script>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Barangay Management System</title>
-
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css.map" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/cdbootstrap/css/cdb.min.css" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
-  <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
-
-
   <style>
-    /* The top navigation bar */
-    .header {
-      overflow: hidden;
-      background-color: #002a5b;
-      position: fixed;
-      top: 0;
-      width: 100%;
-      z-index: 1000;
-      /* Ensure the top bar is above other elements */
-      display: flex;
-      align-items: center;
-      padding: 5px 20px;
+    *{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 
-    }
+body{
+    font-family: 'Poppins', sans-serif;
+    overflow: hidden;
+}
 
-    .header img {
-      height: 50px; /* Adjust the height as needed */
-      width: auto;
-    }
+.wave{
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  height: 100%;
+  z-index: -1;
+}
 
-    .header .title {
-      margin-left: 20px;
-      color: white;
-    }
+.container{
+    width: 100vw;
+    height: 100vh;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap :7rem;
+    padding: 0 2rem;
+}
 
-    .header a {
-      color: white;
-      text-decoration: none;
-      padding: 10px;
-      margin-left: auto;
-    }
+.img{
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
 
-    .header a:hover {
-      background-color: #ddd;
-      color: black;
-    }
+.login-content{
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  text-align: center;
+}
 
-    /* The side navigation menu */
-    .sidebar {
-      margin: 60px 0 0 0;
-      padding: 0; /* Adjusted to accommodate the top bar */
-      width: 250px;
-      background-color: #ffffff;
-      position: fixed;
-      height: 100%;
-      overflow-y: auto;
-    }
+.img img{
+  width: 500px;
+}
 
-    /* Sidebar links */
-    .sidebar a {
-      display: block;
-      color: black;
-      padding: 16px;
-      text-decoration: none;
-    }
+form{
+  width: 360px;
+}
 
-    /* Active/current link */
-    .sidebar a.active {
-      background-color: #002a5b;
-      /*background-color: #04AA6D;*/
-      color: white;
-    }
+.login-content img{
+    height: 100px;
+}
 
-    /* Links on mouse-over */
-    .sidebar a:hover:not(.active) {
-      background-color: #002a5b;
-    /* background-color: #555;*/
-    color: white;
-  }
+.login-content h2{
+  margin: 15px 0;
+  color: #333;
+  text-transform: uppercase;
+  font-size: 2.9rem;
+}
 
-div.content {
-      margin-left: 250px;
-      padding: 1px 16px;
-      min-height: 100vh;
-      background-color: #f1f1f1;
-    }
+.login-content .input-div{
+  position: relative;
+    display: grid;
+    grid-template-columns: 7% 93%;
+    margin: 25px 0;
+    padding: 5px 0;
+    border-bottom: 2px solid #d9d9d9;
+}
 
-    /* On screens that are less than 1000px wide, make the sidebar collapsible */
-    @media screen and (max-width: 1000px) {
-      .sidebar {
-        width: 0;
-      }
+.login-content .input-div.one{
+  margin-top: 0;
+}
 
-      .sidebar.show {
-        width: 250px;
-      }
+.i{
+  color: #d9d9d9;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
-      .sidebar a {
-        padding: 16px;
-        text-align: center;
-      }
+.i i{
+  transition: .3s;
+}
 
-      .header .menu-icon {
-        display: block;
-      }
+.input-div > div{
+    position: relative;
+  height: 45px;
+}
 
-      .header a {
-        display: none;
-      }
+.input-div > div > h5{
+  position: absolute;
+  left: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+  color: #999;
+  font-size: 18px;
+  transition: .3s;
+}
 
-      .header .title {
-        margin-left: 0;
-      }
+.input-div:before, .input-div:after{
+  content: '';
+  position: absolute;
+  bottom: -2px;
+  width: 0%;
+  height: 2px;
+  background-color: #38d39f;
+  transition: .4s;
+}
 
-      .header .title,
-      .header .menu-icon {
-        cursor: pointer;
-        order: -1;
-      }
+.input-div:before{
+  right: 50%;
+}
 
-      div.content {
-        margin-left: 0;
+.input-div:after{
+  left: 50%;
+}
+
+.input-div.focus:before, .input-div.focus:after{
+  width: 50%;
+}
+
+.input-div.focus > div > h5{
+  top: -5px;
+  font-size: 15px;
+}
+
+.input-div.focus > .i > i{
+  color: #38d39f;
+}
+
+.input-div > div > input{
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  border: none;
+  outline: none;
+  background: none;
+  padding: 0.5rem 0.7rem;
+  font-size: 1.2rem;
+  color: #555;
+  font-family: 'poppins', sans-serif;
+}
+
+.input-div.pass{
+  margin-bottom: 4px;
+}
+
+a{
+  display: block;
+  text-align: right;
+  text-decoration: none;
+  color: #999;
+  font-size: 0.9rem;
+  transition: .3s;
+}
+
+a:hover{
+  color: #38d39f;
+}
+
+.btn{
+  display: block;
+  width: 100%;
+  height: 50px;
+  border-radius: 25px;
+  outline: none;
+  border: none;
+  background-image: linear-gradient(to right, #32be8f, #38d39f, #32be8f);
+  background-size: 200%;
+  font-size: 1.2rem;
+  color: #fff;
+  font-family: 'Poppins', sans-serif;
+  text-transform: uppercase;
+  margin: 1rem 0;
+  cursor: pointer;
+  transition: .5s;
+}
+.btn:hover{
+  background-position: right;
+}
+
+
+@media screen and (max-width: 1050px){
+  .container{
+    grid-gap: 5rem;
   }
 }
 
-</style>
+@media screen and (max-width: 1000px){
+  form{
+    width: 290px;
+  }
 
-</head>
+  .login-content h2{
+        font-size: 2.4rem;
+        margin: 8px 0;
+  }
 
+  .img img{
+    width: 400px;
+  }
+}
 
-<body style="background: url('img/') no-repeat center center fixed; background-size: 100%;" onload="startTime()">
+@media screen and (max-width: 900px){
+  .container{
+    grid-template-columns: 1fr;
+  }
 
-  <!-- Top Navigation Bar -->
-  <div class="header">
-    <img src="img/logo.png" alt="Iloilo City Seal">
-    <div class="title">
-      <span class="h4">Barangay Buntatala</span><br>
-      <span class="d-none d-md-block">Jaro, Iloilo City</span>
-      <span class="d-none d-sm-none">Jaro, Iloilo City</span>
-    </div>
-    <div id="txt" style="color: white; margin-left: auto; text-align: center;"></div>
-  </div>
+  .img{
+    display: none;
+  }
 
-  <!-- Sidebar -->
-  <div class="sidebar">
+  .wave{
+    display: none;
+  }
 
-    <a href="#dashboard.php">
-           <i class="fas fa-home"></i>
-            <span class="nav-item">Dashboard</span>
-          </a>
-    <a href="#announcement.php">
-            <i class="fas fa-bullhorn"></i>
-            <span class="nav-item">Announcement</span>
-          </a>
-          <a href="#officials.php">
-            <i class="fas fa-user"></i>
-            <span class="nav-item">Officials</span>
-          </a>
-    <a href="#issuanceDet.php">
-            <i class="fas fa-id-card"></i>
-            <span class="nav-item">Issuance Details</span>
-          </a>
-          <a href="#contentM.php">
-            <i class="fas fa-database"></i>
-            <span class="nav-item">Content Management</span>
-          </a>
-    <a href="#records.php">
-            <i class="fab fa-dochub"></i>
-            <span class="nav-item">Records</span>
-          </a>
-          <a href="#request.php">
-            <i class="fas fa-folder-open"></i>
-            <span class="nav-item">Monitoring of Request</span>
-          </a>
-    <a href="#setting.php">
-            <i class="fas fa-cog"></i>
-            <span class="nav-item">Setting</span>
-          </a>
-          <a href="#help.php">
-            <i class="fas fa-question-circle"></i>
-            <span class="nav-item">Help</span>
-          </a>
-    <a href="#logout.php">
-            <i class="fas fa-sign-out-alt"></i>
-            <span class="nav-item">Logout</span>
-          </a>
-  </div>
+  .login-content{
+    justify-content: center;
+  }
+}
+  </style>
 
-  <!-- Page content -->
-  <div class="content">
-    Dashboard |  
-  </div>
-
-  <!-- Script for time -->
   <script>
-    function startTime() {
-      var today = new Date();
-      var day = today.getDate();
-      var monthNames = ["January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"];
-      var month = monthNames[today.getMonth()];
-      var year = today.getFullYear();
-      var h = today.getHours();
-      var m = today.getMinutes();
-      var s = today.getSeconds();
-      var ampm = h >= 12 ? 'pm' : 'am';
+    const inputs = document.querySelectorAll(".input");
 
-      if (h > 12) {
-        h = h - 12;
-      } else if (h == 0) {
-        h = 12;
-      }
 
-      m = checkTime(m);
-      s = checkTime(s);
-      document.getElementById('txt').innerHTML = month + " " + day + ", " + year + " <br> " + h + ":" + m + ":" + s + " " + ampm;
-      var t = setTimeout(startTime, 500);
-    }
+function addcl(){
+  let parent = this.parentNode.parentNode;
+  parent.classList.add("focus");
+}
 
-    function checkTime(i) {
-      if (i < 10) { i = "0" + i };
-      return i;
-    }
+function remcl(){
+  let parent = this.parentNode.parentNode;
+  if(this.value == ""){
+    parent.classList.remove("focus");
+  }
+}
+
+
+inputs.forEach(input => {
+  input.addEventListener("focus", addcl);
+  input.addEventListener("blur", remcl);
+});
+
+//Source :- https://github.com/sefyudem/Responsive-Login-Form/blob/master/img/avatar.svg
   </script>
-
+</head>
+<body>
+  <img class="wave" src="https://raw.githubusercontent.com/sefyudem/Responsive-Login-Form/master/img/wave.png">
+  <div class="container">
+    <div class="img">
+      <img src="https://raw.githubusercontent.com/sefyudem/Responsive-Login-Form/master/img/bg.svg">
+    </div>
+    <div class="login-content">
+      <form>
+        <img src="https://raw.githubusercontent.com/sefyudem/Responsive-Login-Form/master/img/avatar.svg">
+        <h2 class="title">Welcome</h2>
+              <div class="input-div one">
+                 <div class="i">
+                    <i class="fas fa-user"></i>
+                 </div>
+                 <div class="div">
+                    <h5>Username</h5>
+                    <input type="text" class="input">
+                 </div>
+              </div>
+              <div class="input-div pass">
+                 <div class="i"> 
+                    <i class="fas fa-lock"></i>
+                 </div>
+                 <div class="div">
+                    <h5>Password</h5>
+                    <input type="password" class="input">
+                 </div>
+              </div>
+              <a href="#">Forgot Password?</a>
+              <input type="submit" class="btn" value="Login">
+            </form>
+        </div>
+    </div>
+    
 </body>
 </html>
