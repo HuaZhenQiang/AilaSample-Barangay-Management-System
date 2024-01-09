9bdd66a0cbcfd019d818a74656747ad8f734a897
HuaZhenQiang <irajude.tajanlangit@wvsu.edu.ph>
2024-01-10 07:59:36 +0800

    Update request.php

---
+++ request.php
@@ -0,0 +1,32 @@
+<?php 
+include 'sample.php';
+include 'connection.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>BBJMS</title>
+
+	<!-- <style>
+		body ,h1 {
+
+			margin-left: 10%;
+			margin-top: 5%;
+		}
+	</style> -->
+</head>		
+<body>
+	<div class="home-section">
+      <div class="home-content">
+      <!-- <i class="fas fa-bars"></i>
+        <span class="text">BMS</span> -->
+	<h1 >Sample Request</h1>
+      </div>
+    </div>
+
+
+</body>
+</html>
