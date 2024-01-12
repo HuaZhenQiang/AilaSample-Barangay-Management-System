813750a789678a2951446745f9248e43de25d992
HuaZhenQiang <irajude.tajanlangit@wvsu.edu.ph>
2024-01-13 02:07:50 +0800

    Update admin_login.php

--- connection.php
+++ connection.php
@@ -1,55 +1,21 @@
 <?php
-
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'bms');
+define('host', 'localhost');
+define('user', 'root');
+define('password', '');
+define('dbname', 'bms');
 $pdo = null;
 
+$con = new mysqli('localhost', 'root', '', 'bms');
 
-$con=new mysqli('localhost', 'root', '', 'bms');
-
-if(!$con){
-	die('Connection Failed'.mysqli_connect_error());
-	//die(mysqli_error($con));
+if (!$con) {
+    die('Connection Failed' . mysqli_connect_error());
 }
-
 
 try {
-	$pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME. ";charset=utf8mb4", DB_USER, DB_PASSWORD);
-	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-	echo 'Connection Failed: ' . $e->getMessage();
-	die();
-}
-
-?>
-
-
-
-
-
-
-
-
-
-
-
-
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed'); //prevent direct script access
-$host = 'localhost';
-$user = 'root'; //replace with your database username
-$password = ''; //replace with your database password
-$dbname = 'bms'; //replace with your database name
-$dsn = '';
-
-try{
-    $dsn = 'mysql:host='.$host. ';dbname='.$dbname;
-
-    $pdo = new PDO($dsn, $user, $password);
+    $pdo = new PDO('mysql:host=' . constant('host') . ';dbname=' . constant('dbname') . ';charset=utf8mb4', constant('user'), constant('password'));
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch(PDOException $e){
-    echo 'connection failed: '.$e->getMessage();
+} catch (PDOException $e) {
+    echo 'Connection Failed: ' . $e->getMessage();
+    die();
 }
 ?>
